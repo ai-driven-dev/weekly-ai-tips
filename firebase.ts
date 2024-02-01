bdb6dcf85b664517f3be442533db6d908e98085e
Alex <contact.alexsoyes@gmail.com>
2024-02-01 12:58:37 +0100

    build: add commit with conventional messages, lint, tests, build, hooks, dependabot

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import * as admin from 'firebase-admin';
+import * as admin from "firebase-admin";
 import { initializeApp } from "firebase/app";
 
 // Now you can use `db` to interact with Firestore, or use other Firebase services.
@@ -16,23 +16,21 @@ const firebaseConfig = {
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
-  measurementId: "G-0D13P153D2"
+  measurementId: "G-0D13P153D2",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
-if (!admin.apps.length) {
+if (admin.apps.length === 0) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
   });
 }
 
-export {
-  admin, app
-};
+export { admin, app };
