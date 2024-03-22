0c87e341cc0199fc55f4a7b4fecad60f14405f35
Alex <contact.alexsoyes@gmail.com>
2024-03-22 07:17:21 +0100

    fix: remove tips fetching in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
-import { fetchTips } from "@/src/features/tipsManagement/api/fetchTips";
-import type TipEntity from "@/src/features/tipsManagement/types/TipEntity";
 import Link from "next/link";
 import React from "react";
 
-async function getData(): Promise<Array<Partial<TipEntity>>> {
-  const tips = fetchTips();
-
-  return await tips;
-}
-
 export default async function Page(): Promise<React.ReactElement> {
-  const data = await getData();
-
   return (
     <main>
       <nav>
@@ -21,9 +11,6 @@ export default async function Page(): Promise<React.ReactElement> {
           </li>
         </ul>
       </nav>
-      <pre style={{ height: "200px", overflow: "auto" }}>
-        <code>{JSON.stringify(data, null, 2)}</code>
-      </pre>
     </main>
   );
 }
