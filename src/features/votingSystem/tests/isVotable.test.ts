0d246508c5bd1786968ff3e2497104a09faf1f58
Alex <contact.alexsoyes@gmail.com>
2024-04-22 09:10:14 +0200

    test: is tip votable

---
+++ src/features/votingSystem/tests/isVotable.test.ts
@@ -0,0 +1,76 @@
+import TipEntity from "../../tipManagement/types/TipEntity";
+import isVotable from "../utils/isVotable";
+
+describe("isVotable", () => {
+  it('can vote for a tip that has "ready" status', () => {
+    const tip: TipEntity = {
+      title: "Test",
+      description: "Test",
+      content: "Test",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      ownerID: "1",
+      upVotes: 0,
+      downVotes: 0,
+      status: "ready",
+    };
+
+    const result = isVotable(tip);
+
+    expect(result).toBe(true);
+  });
+
+  it("cannot vote for a tip that has status “draft”", () => {
+    const tip: TipEntity = {
+      title: "Test",
+      description: "Test",
+      content: "Test",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      ownerID: "1",
+      upVotes: 0,
+      downVotes: 0,
+      status: "draft",
+    };
+
+    const result = isVotable(tip);
+
+    expect(result).toBe(false);
+  });
+
+  it("can vote if downVotes > -3 or upVotes < 3", () => {
+    const tip: TipEntity = {
+      title: "Test",
+      description: "Test",
+      content: "Test",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      ownerID: "1",
+      upVotes: 2,
+      downVotes: -2,
+      status: "ready",
+    };
+
+    const result = isVotable(tip);
+
+    expect(result).toBe(true);
+  });
+
+  it("cannot vote if downVotes < -3 and upVotes < 3", () => {
+    const tip: TipEntity = {
+      title: "Test",
+      description: "Test",
+      content: "Test",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      ownerID: "1",
+      upVotes: 2,
+      downVotes: -4,
+      status: "ready",
+    };
+
+    const result = isVotable(tip);
+
+    expect(result).toBe(false);
+  });
+});
---
+++ src/features/votingSystem/utils/isVotable.ts
@@ -0,0 +1,11 @@
+import TipEntity from "../../tipManagement/types/TipEntity";
+
+function isVotable(tip: TipEntity): boolean {
+  if (tip.status === "draft" || (tip.downVotes < -3 && tip.upVotes < 3)) {
+    return false;
+  }
+
+  return true;
+}
+
+export default isVotable;
