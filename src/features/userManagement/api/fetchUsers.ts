e82bec49f5ae70a6797afa018abf38f61ded4a7f
Alex <contact.alexsoyes@gmail.com>
2024-06-29 10:43:37 +0200

    refactor: fetch users and voting system's test

--- src/features/userManagement/api/fetchUsers.ts
+++ src/features/userManagement/api/fetchUsers.ts
@@ -1,23 +1,41 @@
 import { db } from '@/firebaseAdmin';
 import UserEntity from '@/src/features/userManagement/types/UserEntity';
 
+/**
+ * Fetches all users from the Firestore database.
+ *
+ * This function retrieves all user documents from the 'users' collection,
+ * converts each document to a UserEntity object, and returns an array of these objects.
+ *
+ * @returns {Promise<UserEntity[]>} A promise that resolves to an array of UserEntity objects.
+ */
 export async function fetchUsers(): Promise<UserEntity[]> {
   const usersCollection = db.collection('users');
   const snapshot = await usersCollection.get();
-  const users: UserEntity[] = [];
+  return snapshot.docs.map((doc) => docToUserEntity(doc));
+}
 
-  snapshot.forEach((doc) => {
-    const user = doc.data();
-    users.push({
-      id: doc.id,
-      name: user.name,
-      picture: user.picture,
-      email: user.email,
-      roles: user.roles,
-      createdAt: user.createdAt.toDate(),
-      updatedAt: user.updatedAt.toDate(),
-    });
-  });
-
-  return users;
+/**
+ * Converts a Firestore document to a UserEntity object.
+ *
+ * This helper function takes a Firestore document, extracts its data,
+ * and maps it to a UserEntity object, including converting Firestore Timestamps
+ * to JavaScript Date objects for the createdAt and updatedAt fields.
+ *
+ * @param {FirebaseFirestore.QueryDocumentSnapshot} doc - The Firestore document to convert.
+ * @returns {UserEntity} The converted UserEntity object.
+ */
+function docToUserEntity(
+  doc: FirebaseFirestore.QueryDocumentSnapshot,
+): UserEntity {
+  const data = doc.data();
+  return {
+    id: doc.id,
+    name: data.name,
+    picture: data.picture,
+    email: data.email,
+    roles: data.roles,
+    createdAt: data.createdAt.toDate(),
+    updatedAt: data.updatedAt.toDate(),
+  };
 }
--- src/features/votingSystem/tests/votingSystem.spec.ts
+++ src/features/votingSystem/tests/votingSystem.spec.ts
@@ -3,6 +3,15 @@ import UserEntity from '../../userManagement/types/UserEntity';
 import { getUserVotes } from '../api/getUserVotes';
 import { vote } from '../api/vote';
 
+// Constants for reuse in tests
+const TIP_ID = 'tip1';
+const USER_ID = 'user1';
+const VOTE_TYPE_UPVOTE = 'upvote';
+const ERROR_USER_ALREADY_VOTED = 'User has already voted on the tip';
+const ERROR_USER_OWN_TIP = 'User cannot vote on their own tip';
+const ERROR_TIP_NOT_VOTABLE = 'Tip is not votable!';
+
+// Simplified mock setup
 jest.mock('../api/getUserVotes');
 jest.mock('@/firebaseAdmin', () => ({
   db: {
@@ -14,141 +23,74 @@ jest.mock('@/firebaseAdmin', () => ({
 jest.mock('../../tipManagement/api/editTipVoteCounts');
 
 describe('Voting for Tips', () => {
+  let tipData: TipEntity;
+  let fromUser: UserEntity;
+
   beforeEach(() => {
     jest.clearAllMocks();
-  });
 
-  test('should not allow a user to vote twice for the same tip', async () => {
-    // Arrange
-    const tipData: TipEntity = {
-      id: 'tip1',
+    // Common test data setup
+    tipData = {
+      id: TIP_ID,
       title: 'Tip 1',
       description: 'Description 1',
       content: 'Content 1',
       createdAt: new Date(),
       updatedAt: new Date(),
-      ownerID: 'user2',
+      ownerID: 'user2', // Default to a different user
       upVotes: 0,
       downVotes: 0,
       status: 'ready',
       slug: 'tip-1',
       tagIDs: ['tag1'],
     };
-    const fromUser: UserEntity = {
-      id: 'user1',
+
+    fromUser = {
+      id: USER_ID,
       name: 'User 1',
       email: 'user1@example.com',
       roles: ['MEMBER'],
       createdAt: new Date(),
       updatedAt: new Date(),
     };
-    const voteType = 'upvote';
+  });
+
+  test('should not allow a user to vote twice for the same tip', async () => {
+    // Mock setup for this test
     (getUserVotes as jest.Mock).mockResolvedValueOnce([{ id: 'vote1' }]);
 
-    // Act & Assert
-    await expect(vote(tipData, fromUser, voteType)).rejects.toThrow(
-      'User has already voted on the tip',
+    // Attempt to vote
+    await expect(vote(tipData, fromUser, VOTE_TYPE_UPVOTE)).rejects.toThrow(
+      ERROR_USER_ALREADY_VOTED,
     );
   });
 
   test('should not allow a user to vote on their own tip', async () => {
-    // Arrange
-    const tipData: TipEntity = {
-      id: 'tip1',
-      title: 'Tip 1',
-      description: 'Description 1',
-      content: 'Content 1',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      ownerID: 'user1',
-      upVotes: 0,
-      downVotes: 0,
-      status: 'ready',
-      slug: 'tip-1',
-      tagIDs: ['tag1'],
-    };
-    const fromUser: UserEntity = {
-      id: 'user1',
-      name: 'User 1',
-      email: 'user1@example.com',
-      roles: ['MEMBER'],
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-    const voteType = 'upvote';
+    // Adjusting tip owner to simulate user's own tip
+    tipData.ownerID = USER_ID;
     (getUserVotes as jest.Mock).mockResolvedValueOnce([]);
 
-    // Act & Assert
-    await expect(vote(tipData, fromUser, voteType)).rejects.toThrow(
-      'User cannot vote on their own tip',
+    await expect(vote(tipData, fromUser, VOTE_TYPE_UPVOTE)).rejects.toThrow(
+      ERROR_USER_OWN_TIP,
     );
   });
 
   test('should not allow a user to vote on a non-ready tip', async () => {
-    // Arrange
-    const tipData: TipEntity = {
-      id: 'tip1',
-      title: 'Tip 1',
-      description: 'Description 1',
-      content: 'Content 1',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      ownerID: 'user2',
-      upVotes: 0,
-      downVotes: 0,
-      status: 'draft',
-      slug: 'tip-1',
-      tagIDs: ['tag1'],
-    };
-    const fromUser: UserEntity = {
-      id: 'user1',
-      name: 'User 1',
-      email: 'user1@example.com',
-      roles: ['MEMBER'],
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-    const voteType = 'upvote';
+    // Adjusting tip status to simulate non-votable condition
+    tipData.status = 'draft';
     (getUserVotes as jest.Mock).mockResolvedValueOnce([]);
 
-    // Act & Assert
-    await expect(vote(tipData, fromUser, voteType)).rejects.toThrow(
-      'Tip is not votable!',
+    await expect(vote(tipData, fromUser, VOTE_TYPE_UPVOTE)).rejects.toThrow(
+      ERROR_TIP_NOT_VOTABLE,
     );
   });
 
   // eslint-disable-next-line @typescript-eslint/quotes
   test("should accept a user's first vote on a ready tip", async () => {
-    // Arrange
-    const tipData: TipEntity = {
-      id: 'tip1',
-      title: 'Tip 1',
-      description: 'Description 1',
-      content: 'Content 1',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      ownerID: 'user2',
-      upVotes: 0,
-      downVotes: 0,
-      status: 'ready',
-      slug: 'tip-1',
-      tagIDs: ['tag1'],
-    };
-    const fromUser: UserEntity = {
-      id: 'user1',
-      name: 'User 1',
-      email: 'user1@example.com',
-      roles: ['MEMBER'],
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-    const voteType = 'upvote';
+    // Default mock setup applies
     (getUserVotes as jest.Mock).mockResolvedValueOnce([]);
 
-    // Act
-    const result = await vote(tipData, fromUser, voteType);
-
-    // Assert
+    const result = await vote(tipData, fromUser, VOTE_TYPE_UPVOTE);
     expect(result).toBe(true);
   });
 });
