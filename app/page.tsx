eb4830ca1bc591d6ce43d8ffd0bcaeb82c98bff0
Alex <contact.alexsoyes@gmail.com>
2024-08-20 07:39:27 +0200

    feat(page): add Newsletter component to HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import Newsletter from '@/src/features/public/components/Newsletter';
 import PublicHeader from './header';
 
 export default async function Page(): Promise<React.ReactElement> {
   return (
     <>
       <PublicHeader />
-      <main>v0 incoming</main>
+      <main>
+        <Newsletter />
+      </main>
     </>
   );
 }
